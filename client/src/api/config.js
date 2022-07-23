ae63e37ea1190ffc0b9118c2af97fb8e9df49605
onethps <onethps@gmail.com>
2022-07-23 22:21:04 +0300

    fix adaptive desgn

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const settings = {
-
   withCredentials: true,
 };
 
 export const instance = axios.create({
-  baseURL: 'https://mern-simple-video-hosting.herokuapp.com/api/',
+  baseURL: '/api/',
   ...settings
 
 })
